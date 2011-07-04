815705477b3d065a0c26f023bdafc6dbe1141447
Byju Veedu <byjupv@gmail.com>
2011-07-04 17:36:06 +0530

    separated css and js files

---
+++ js/easyref.js
@@ -0,0 +1,40 @@
+		$(function() {
+				$('#tabs').tabs();
+			    $('#intro').click(function(){
+			        $('#overlay').fadeIn('fast',function(){
+			            $('#box').show();
+			        });
+			    });
+			    $('#boxclose').click(function(){
+			    	  $('#overlay').fadeOut('fast',function(){
+				            $('#box').hide();
+				        });
+			    });
+
+			});
+	   $(function() {
+		var availableTags = [
+			"Computer Science",
+			"General",
+			"Mathematics",
+			"Programming",
+			"C",
+			"C++",
+			"Clojure",
+			"Erlang",
+			"Fortran",
+			"Groovy",
+			"Haskell",
+			"Java",
+			"JavaScript",
+			"Lisp",
+			"Perl",
+			"PHP",
+			"Python",
+			"Ruby",
+			"Scala"
+		];
+		$( "#tags" ).autocomplete({
+			source: availableTags
+		});
+	});
